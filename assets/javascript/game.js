55130e5046c46647fa55932f393ebe0b367d4cdd
Meredith <dinsdale.meredith@gmail.com>
2019-02-09 14:38:51 -0800

    homework ready for submission

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 
-//declare variables-----------------------------------------------------
+//global variables-----------------------------------------------------
 
 var alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L',
     'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
@@ -9,33 +9,44 @@ var wins = 0;
 var losses = 0;
 var lettersArray = [];
 
+var crystalBall = $(".crystal-ball");
+
 console.log(randomLetter);
+var audioElement = document.createElement("audio");
+    audioElement.setAttribute("src", "assets/javascript/Twinkle-sound-effect.mp3");
 
-
-//When user presses a letter key, the letter is logged as userGuess
+$(document).ready(function() {
+//onKey event triggerred when user presses letter key ---------------------------------------------------------------------
 
     document.onkeyup=function(event) {
         var userGuess = event.key.toUpperCase();
         console.log(userGuess);
         if (Alphanumeric(event.key)){ 
             document.getElementById("message").innerHTML ="";
-        
+            crystalBall.animate({ opacity: "0" }, 1000);
+//If user wins-------------------------------------------------------------------------------------------------------------      
         if (userGuess === randomLetter) {
             wins +=1;
             console.log(wins);
-            document.getElementById("message").innerHTML ="Whoa, you read my mind!    I was thinking of the letter " + randomLetter + ".  Nice!   Guess a letter to test your psychic skills again.";
+            crystalBall.animate({ opacity: ".6" }, 500);
+
+            setTimeout(function(){ audioElement.play(); }, 100);
+            
+            document.getElementById("message").innerHTML ="<h5>You must have a crystal ball!</h5>  <h5> I was thinking of the letter " + randomLetter + ". </5> <h5> I'm thinking of another letter if you're ready to try again.</h5>";
             guessCounter = 0;
             lettersArray = [];
             randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
             console.log(randomLetter);
-               
             }
+//If guess is incorrect----------------------------------------------------------------------------------------------------
+            
         else {
             guessCounter ++;
             lettersArray.push(userGuess);
-        
+
+//If user loses------------------------------------------------------------------------------------------------------------       
                 if (guessCounter >= 10) {
-                    document.getElementById("message").innerHTML ="You're all out of guesses...  Looks like your ESP is on the fritz!  I'm thinking of another letter if you're ready to try again...";
+                    document.getElementById("message").innerHTML ="<h5>You're all out of guesses...  Looks like your ESP is on the fritz! </h5> <h5> I'm thinking of another letter if you're ready to try again...</h5>";
                     losses +=1;
                     guessCounter = 0;
                     lettersArray = [];
@@ -63,7 +74,7 @@ document.getElementById("letters").innerHTML = 'Your (incorrect) predictions: ' 
         }
      }  
    
-    
+    });   
 
 
 
